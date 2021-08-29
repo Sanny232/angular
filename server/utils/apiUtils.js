c4c348e0d95a9979489908961b7b2da4d49bdfc4
Alexander Vasyliev <gogleuser67967@gmail.com>
2021-08-29 16:25:00 +0300

    clear

--- server/utils/apiUtils.js
+++
@@ -1,9 +0,0 @@
-const asyncWrapper = (callback) => {
-  return (req, res, next) =>
-    callback(req, res)
-      .catch(next);
-};
-
-module.exports = {
-  asyncWrapper,
-};
--- src/app/features/games/pipes/shortened.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShortenedPipe } from './shortened.pipe';
-
-describe('ShortenedPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ShortenedPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
